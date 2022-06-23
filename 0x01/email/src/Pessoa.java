03742edc0c7f085878a642073d6f470f34e3dfe2
Thomaz França <thomazf@ciandt.com>
2022-06-23 16:58:49 -0300

    JUnit - Parte 1

---
+++ 0x01/email/src/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa {
+    public static boolean emailValid(String email){
+        if (email.contains("@") && email.length() <= 50 ){
+            return true;
+        }
+        return false;
+    }
+}
