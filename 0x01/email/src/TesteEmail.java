a329845c30e124e4a9db44e42ccb6f42f5070a0d
Thomaz França <thomazf@ciandt.com>
2022-06-23 17:10:11 -0300

    JUnit - Parte 1

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,21 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+
 public class TesteEmail {
-
-
-    public static boolean testar_email_com_arroba() {
-        return Pessoa.emailValid("email_teste@dominio.com.br");
-
-    }
-    public static boolean testar_email_mais_50_caracteres() {
-
-        return Pessoa.emailValid("email_teste@dominio.com.br");
+    @Test
+    public void testar_email_com_arroba() {
+        assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
     }
 
-    public static boolean testar_email_sem_arroba() {
-        return Pessoa.emailValid("email_teste@dominio.com.br");
+    @Test void testar_email_sem_arroba() {
+        assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
 
     }
+    @Test
+    public void testar_email_mais_50_caracteres() {
+        assertEquals(false, Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+    }
+
 
 }
-
-
-
